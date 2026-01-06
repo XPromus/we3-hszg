d3275643d77b405d579e94f65abdfc5a88470633
Christopher Hilgner <hilgi45@gmail.com>
2026-01-06 09:45:58 +0100

    CORS config. Issues in production.

--- todo-list/backend/src/main/kotlin/com/hszg/todolist/config/CorsConfiguration.kt
+++ todo-list/backend/src/main/kotlin/com/hszg/todolist/config/CorsConfiguration.kt
@@ -11,7 +11,7 @@ class CorsConfiguration: WebMvcConfigurer {
         registry: CorsRegistry
     ) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:5173")
+            .allowedOrigins("http://localhost:3000/, http://localhost:5173")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
     }
 
